edaca18af60cbec17647bf574594c276d6d68ed0
Teacher <shaojung@gmail.com>
2018-01-10 10:39:45 +0800

    Second commit.

--- app/src/main/java/tw/com/pcschool/dd2018011001/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018011001/MainActivity.java
@@ -1,13 +1,44 @@
 package tw.com.pcschool.dd2018011001;
 
+import android.os.Handler;
+import android.os.Trace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Handler handler;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
+        tv = findViewById(R.id.textView);
+        MyThread t = new MyThread();
+        t.start();
+    }
+
+    class MyThread extends Thread
+    {
+        @Override
+        public void run() {
+            super.run();
+            while(true)
+            {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(String.valueOf(Integer.valueOf(tv.getText().toString())+1));
+                    }
+                });
+
+            }
+        }
     }
 }
